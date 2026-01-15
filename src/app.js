adc53b295dfa49eea64d2812bd0a065232fa3ba3
Prachi Mulay <prachimulay17@gmail.com>
2026-01-15 11:13:28 +0530

    udated app.js

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(helmet({
 
 // CORS configuration
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: process.env.CLIENT_URL,
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
