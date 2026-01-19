e210b8632b92094f74abbf834c7a6bb4f998b978
Prachi Mulay <prachimulay17@gmail.com>
2026-01-19 23:20:46 +0530

    sendemail.js

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -3,14 +3,18 @@ import nodemailer from "nodemailer";
 export const sendEmail = async (to, subject, html) => {
   try {
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false, // MUST be false for 587
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-      }
-    });
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  },
+  connectionTimeout: 10000, // ⏱️ 10 seconds
+  greetingTimeout: 10000,
+  socketTimeout: 10000
+});
+
 
     // 🔥 THIS IS IMPORTANT
     await transporter.verify();
